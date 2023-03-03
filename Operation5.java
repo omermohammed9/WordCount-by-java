b3d797a57d14ba47995b98fcb8d062e20ff781e7
scorpiraq <96728173+scorpiraq@users.noreply.github.com>
2023-03-03 22:44:52 +0300

    Update Operation5.java

--- Operation5.java
+++ Operation5.java
@@ -18,38 +18,40 @@ public class Operation5 { //server 4
 			DataOutputStream dout = new DataOutputStream(out);
 			BufferedOutputStream bout = new BufferedOutputStream(dout);
 			do {
-			txt = din.readUTF();
-			System.out.println(txt);	
-			txt = txt.toLowerCase();
-			char[] chars = txt.toCharArray();
-			int count = 0;
-			for (int i = 0; i <txt.length(); i++) {
-				if (txt.charAt(i) == 'o') {
-					count++;
+				txt = din.readUTF();
+				System.out.println(txt);	
+				txt = txt.toLowerCase();
+				char[] chars = txt.toCharArray();
+				int count = 0;
+				for (int i = 0; i <txt.length(); i++) {
+					if (txt.charAt(i) == 'o') {
+						count++;
+					}
 				}
-			}
-			System.out.println("Total no of vowels (o) in string are: " + count);
-			
-			int counter = 0;
-			char ch[] = new char [txt.length()];
-			for (int i =0; i < txt.length(); i++) {
-				ch[i]= txt.charAt(i);  
-				 if( ((i>0)&&(ch[i]!=' ')&&(ch[i-1]==' ')) || ((ch[0]!=' ')&&(i==0)) ) { 
-	                 counter++;  
-				 }
-			}
-			System.out.println("number of words: " + counter);
-			dout.writeInt(count);
-			dout.flush();
-			
-			dout.writeInt(counter);
-			dout.flush();
-			
-			}while(true);
-		}catch (Exception e) {
+				System.out.println("Total no of vowels (o) in string are: " + count);
+				
+				int counter = 0;
+				char ch[] = new char [txt.length()];
+				for (int i =0; i < txt.length(); i++) {
+					ch[i]= txt.charAt(i);  
+					if( ((i>0)&&(ch[i]!=' ')&&(ch[i-1]==' ')) || ((ch[0]!=' ')&&(i==0)) ) { 
+						counter++;  
+					}
+				}
+				System.out.println("number of words: " + counter);
+				
+				// Send both values in a single message
+				int[] values = {count, counter};
+				dout.writeInt(values.length);
+				for (int i = 0; i < values.length; i++) {
+					dout.writeInt(values[i]);
+				}
+				dout.flush();
+				
+			} while (true);
+		} catch (Exception e) {
 			System.err.println(e.getMessage());
 		}
-
 	}
-
 }
+
